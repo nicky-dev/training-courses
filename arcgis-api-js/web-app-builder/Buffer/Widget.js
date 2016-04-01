64a21fe1bd27edbbadab09860523320a48bf68fb
PASSAPOL EAMSAKUL <passapol.e@gmail.com>
2016-04-02 01:24:28 +0700

    create

---
+++ arcgis-api-js/web-app-builder/Buffer/Widget.js
@@ -0,0 +1,33 @@
+define([
+    "dojo/_base/declare",
+    "jimu/BaseWidget"
+], function (
+    declare,
+    BaseWidget
+    ) {
+    var clazz = declare([
+        BaseWidget
+    ], {
+
+        postCreate: function () {
+            this.inherited(arguments);
+        },
+
+        startup: function () {
+        },
+
+        onActive: function () {
+
+        },
+        onDeactive: function () {
+
+        },
+        onOpen: function () {
+
+        },
+        onClose: function () {
+
+        }
+    });
+    return clazz;
+});
