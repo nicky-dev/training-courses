abfb406e47bb9f0ccb8ed8597f977ccccb7400b1
PASSAPOL EAMSAKUL <passapol.e@gmail.com>
2016-04-02 16:02:17 +0700

    complete

--- arcgis-api-js/web-app-builder/Buffer/Widget.js
+++ arcgis-api-js/web-app-builder/Buffer/Widget.js
@@ -1,16 +1,82 @@
 define([
     "dojo/_base/declare",
-    "jimu/BaseWidget"
+    "jimu/BaseWidget",
+    "dijit/_WidgetsInTemplateMixin",
+    "jimu/LayerInfos/LayerInfos",
+    "dojo/store/Memory",
+    "dojo/_base/lang",
+
+    "esri/geometry/geometryEngine",
+    "esri/graphic",
+
+    "esri/symbols/SimpleFillSymbol",
+    "esri/symbols/SimpleLineSymbol",
+    "esri/Color"
 ], function (
     declare,
-    BaseWidget
+    BaseWidget,
+    _WidgetsInTemplateMixin,
+    LayerInfos,
+    Memory,
+    lang,
+
+    geometryEngine,
+    Graphic,
+
+    SimpleFillSymbol,
+    SimpleLineSymbol,
+    Color
     ) {
     var clazz = declare([
-        BaseWidget
+        BaseWidget,
+        _WidgetsInTemplateMixin
     ], {
 
         postCreate: function () {
             this.inherited(arguments);
+
+            var store = new Memory();
+            LayerInfos.getInstance(this.map, this.map.itemInfo).then(function (layerInfosObject) {
+                layerInfosObject.getLayerInfoArray().forEach(function (layerInfo) {
+                    store.add(layerInfo);
+                });
+            });
+
+            this.fltLayerList.set({
+                searchAttr: "title",
+                store: store
+            });
+
+            this.btnBuffer.on("click", lang.hitch(this, function () {
+                if (this._clickHandler) {
+                    this._clickHandler.remove();
+                    this._clickHandler = null;
+                } else {
+                    this._clickHandler = this.map.on("click", lang.hitch(this, function (evt) {
+                        var layerId = this.fltLayerList.get("value");
+                        var radius = this.txtRadius.get("value");
+                        var layer = this.map.getLayer(layerId);
+                        console.log("layer", layer);
+                        console.log("map:click", evt, radius);
+                        var geoBuffer = geometryEngine.buffer(evt.mapPoint, radius, "meters");
+
+                        var sfs = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
+                            new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT,
+                            new Color([255, 0, 0]), 2), new Color([255, 255, 0, 0.25]));
+
+                        var bufferGraphic = new Graphic(geoBuffer, sfs);
+
+                        this.map.graphics.clear();
+                        this.map.graphics.add(bufferGraphic);
+
+                        layer.queryFeatures({
+                            geometry: geoBuffer
+                        }).then(lang.hitch(this, function (response) {
+                            console.log("layer.query", response);
+                        }));
+                    }));
+                }
+            }));
         },
 
         startup: function () {
@@ -20,7 +86,10 @@ define([
 
         },
         onDeactive: function () {
-
+            if (this._clickHandler) {
+                this._clickHandler.remove();
+                this._clickHandler = null;
+            }
         },
         onOpen: function () {
 
